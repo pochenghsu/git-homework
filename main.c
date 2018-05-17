2abb86c6400c2d0fc734e8597709248c2a04d008
pochenghsu <xxxx77799@gmail.com>
2018-05-17 21:40:27 +0800

    1

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
 	scanf("%d",&interger1);
 	scanf("%d",&interger2);
 	interger3=interger1+interger2;
-	printf("和為%d\n",interger1);
+	printf("和為%d\n",interger3);
 	
 	
 	return 0;
