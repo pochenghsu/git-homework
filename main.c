e1f58b4b29557764279cd7edd09b21371e7fa122
pochenghsu <xxxx77799@gmail.com>
2018-05-17 21:05:26 +0800

    123

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a,b,c;
+	printf("輸入兩個數字\n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	c=a+b;
+	printf("和為%d\n",c);
+	
+	
+	return 0;
+}
