43ad4cd9bdf28bc0e2b8f1bf4ff4bbff2f282a37
pochenghsu <xxxx77799@gmail.com>
2018-05-17 21:55:17 +0800

    3

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	int interGer1,interGer2,interGer3;
-	printf("輸入兩個數字\n");
+	int interGer1,interGer2,interGer3; 
+	printf("輸入兩個數字\n"); 
 	scanf("%d",&interGer1);
 	scanf("%d",&interGer2);
 	interGer3=interGer1+interGer2;
